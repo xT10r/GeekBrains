0e28a533bd3411e85e69f68da0e6a331fa063fc8
Alex <j03ef@yandex.ru>
2020-11-29 18:55:55 +0300

    L-06. Обновлено задание 3. Обновлн .gitignore

--- Python/Lesson_06/Tasks/Task06_03.py
+++ Python/Lesson_06/Tasks/Task06_03.py
@@ -20,15 +20,28 @@ class Worker:
 	name: str
 	surname: str
 	position: None
-	_income: dict = dict(wage=wage, bonus=bonus)
+	_income: dict = {"wage": 0, "bonus": 0}
+
+	def __init__(self, name: str, surname: str, position: str, wage: int = 0, bonus: int = 0):
+		self.name = name
+		self.surname = surname
+		self.position = position
+		self._income["wage"] = wage
+		self._income["bonus"] = bonus
 
 
 class Position(Worker):
 
+	def __init__(self, name: str, surname: str, position: str, wage: int = 0, bonus: int = 0):
+		super().__init__(name, surname, position, wage, bonus)
+
 	def get_full_name(self):
-		pass
+		return f"{self.name} {self.surname}"
 
 	def get_total_income(self):
-		pass
+		values = [x for x in self._income.values()]
+		return f"{values[0]} {values[1]}"
 
-	pass
+
+engineer = Position('Петр', 'Петров', 'Бухгалтер', 25000, 5000)
+print(f"{engineer.get_full_name()} {engineer.get_total_income()}")
