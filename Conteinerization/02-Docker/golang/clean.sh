30668a646018e05f6836483b3f3030c126bbd9e1
xT10r <J03eF@yahoo.com>
2021-08-08 23:48:43 +0300

    Conteinerization. Lesson 2
    Добавлены файлы по практическому заданию №2

---
+++ Conteinerization/02-Docker/golang/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+image_name="go-app"
+image_tag="v1"
+
+echo "=============> Stop container"
+docker stop $(docker ps -aq -f ancestor=$image_name:$image_tag)
+
+echo "=============> Clear container"
+docker rm -f $(docker ps -aq -f ancestor=$image_name:$image_tag)
+
+echo "=============> Clear images (1) ..."
+docker rmi -f $(docker images $image_name:$image_tag)
+
+echo "=============> Clear images (2) ..."
+docker rmi -f $(docker images -f dangling=true)
---
+++ Conteinerization/02-Docker/python/clean-build-and-run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+image_name="py-app"
+image_tag="v1"
+
+echo "=============> Stop container"
+docker stop $(docker ps -aq -f ancestor=$image_name:$image_tag)
+
+echo "=============> Clear container"
+docker rm -f $(docker ps -aq -f ancestor=$image_name:$image_tag)
+
+echo "=============> Clear images (1) ..."
+docker rmi -f $(docker images $image_name:$image_tag)
+
+echo "=============> Clear images (2) ..."
+docker rmi -f $(docker images -f dangling=true)
+
+echo "=============> Build image"
+docker build . -t $image_name:$image_tag
+
+echo "=============> Run image"
+docker run -d -p 8080:8080 $image_name:$image_tag
+
+sleep 2
+echo "=============> Curl App"
+result=$(curl --silent -k http://localhost:8080)
+echo $result
+sleep 2
